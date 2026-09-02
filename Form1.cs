317c21da879b191c7c598b89ff42791fadc8fabb
moth-jpg <93683@roc-teraa.nl>
2026-09-02 08:45:39 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizMaster
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source = localhost;Initial Catalog=c#quizmaster; User Id=root;password=''";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
